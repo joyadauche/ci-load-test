150adb89536059ee6df9392a2341dc29a16cce4f
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-10 18:57:49 -0500

    Add command to make workflow fail if a command fails

--- .github/scripts/provision-cluster.sh
+++ .github/scripts/provision-cluster.sh
@@ -1,10 +1,6 @@
+set -e
 cd ./terraform/
-terraform init
+terraform initt
 terraform validate
 terraform plan
 terraform apply -auto-approve
-TERRAFORM_APPLY_EXIT_STATUS="${?}"
-if [[ "${TERRAFORM_APPLY_EXIT_STATUS}" -ne 0 ]]
-then
-    exit 1
-fi
