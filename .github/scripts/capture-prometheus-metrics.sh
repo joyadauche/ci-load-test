a48621df56e2b806c74d2779c764910028a309dd
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-11 15:42:17 -0500

    Check if using sudo to kill a process is allowed

--- .github/scripts/capture-prometheus-metrics.sh
+++ .github/scripts/capture-prometheus-metrics.sh
@@ -1,9 +1,9 @@
-cat ~/.kube/config
 kubectl port-forward svc/kube-prometheus-stack-prometheus 9090:9090 --namespace monitoring --kubeconfig ~/.kube/config&
 curl -sS http://localhost:9090/api/v1/query\?query\=node_load5
+ps aux | grep 9090
 result=$(ps aux | grep 9090 | awk '{print $2}' | tr '\n' ', ' | sed 's/, $//')
 echo "Process Ids: $result"
 for process_id in $(echo $result | sed "s/,/ /g")
 do
-    kill -9 $process_id
+    sudo kill -9 $process_id
 done
