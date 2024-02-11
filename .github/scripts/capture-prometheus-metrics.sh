62bb320d857cc73c6cf476fd84115be690f224c7
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-11 16:13:43 -0500

    Debug capture-prometheus-metrics - check if curl works

--- .github/scripts/capture-prometheus-metrics.sh
+++ .github/scripts/capture-prometheus-metrics.sh
@@ -5,10 +5,10 @@ while ! nc -z localhost 9090; do
     sleep 1
 done
 curl -sS "http://localhost:9090/api/v1/query\?query\=node_load5"
-ps aux | grep 9090
-result=$(ps aux | grep 9090 | awk '{print $2}' | tr '\n' ', ' | sed 's/, $//')
-echo "Process Ids: $result"
-for process_id in $(echo $result | sed "s/,/ /g")
-do
-    sudo kill -9 $process_id
-done
+# ps aux | grep 9090
+# result=$(ps aux | grep 9090 | awk '{print $2}' | tr '\n' ', ' | sed 's/, $//')
+# echo "Process Ids: $result"
+# for process_id in $(echo $result | sed "s/,/ /g")
+# do
+#     sudo kill -9 $process_id
+# done
