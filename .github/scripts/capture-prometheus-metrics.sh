8c7407e94d3d3c1657eb6bddabd47d071b6628df
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-11 16:21:07 -0500

    Remove comments

--- .github/scripts/capture-prometheus-metrics.sh
+++ .github/scripts/capture-prometheus-metrics.sh
@@ -1,14 +1,6 @@
 set -e
-kubectl get svc --namespace monitoring
 kubectl port-forward svc/kube-prometheus-stack-prometheus 9090:9090 --namespace monitoring --kubeconfig ~/.kube/config & 
 while ! nc -z localhost 9090; do
     sleep 1
 done
 curl -sS "http://localhost:9090/api/v1/query\?query\=node_load5"
-# ps aux | grep 9090
-# result=$(ps aux | grep 9090 | awk '{print $2}' | tr '\n' ', ' | sed 's/, $//')
-# echo "Process Ids: $result"
-# for process_id in $(echo $result | sed "s/,/ /g")
-# do
-#     sudo kill -9 $process_id
-# done
