4f289d0c09f43f3bb500e5346181110e7cf872b3
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-11 04:20:31 -0500

    Fix bug that swaps http_req_failed passes and fails. For more details, see issue at https://github.com/grafana/k6/issues/2386

--- tests/load-test.js
+++ tests/load-test.js
@@ -9,9 +9,9 @@ export const options = {
     http_req_duration: ["p(90)<200", "p(95)<300"],
   },
   stages: [
-    { duration: "50s", target: 200 }, 
-    { duration: "5m", target: 200 }, 
-    { duration: "50s", target: 0 },
+    { duration: "30s", target: 50 }, 
+    { duration: "1m", target: 50 }, 
+    { duration: "30s", target: 0 },
   ],
 };
 
@@ -30,21 +30,26 @@ export default function () {
     }
 }
 
-// export function handleSummary(data) {
-//     console.log('Finished executing load tests');
+export function handleSummary(data) {
+    console.log('Finished executing load tests');
 
-//     console.log("Check values:", JSON.stringify(data.metrics))
+    console.log("Check values:", JSON.stringify(data.metrics))
 
-//     const latency = JSON.stringify(data.metrics.http_req_duration.values);
-//     const latency_message = `The latency values are ${latency}\n`;
+    const latency = JSON.stringify(data.metrics.http_req_duration.values);
+    const latency_message = `The latency values are ${latency}\n`;
 
-//     const errors = JSON.stringify(data.metrics.http_req_failed.values);
-//     const errors_message = `The error values are ${errors}\n`;
+    // https://github.com/grafana/k6/issues/2386 - swap http_req_failed passes and fails to fix k6 bug
+    let errors = data.metrics.http_req_failed.values
+    let temp = errors.passes;
+    errors.passes = errors.fails;
+    errors.fails = temp;
+    errors = JSON.stringify(errors);
+    const errors_message = `The error values are ${errors}\n`;
 
-//     const traffic = JSON.stringify(data.metrics.http_reqs.values);
-//     const traffic_message = `The traffic values are ${traffic}\n`;
+    const traffic = JSON.stringify(data.metrics.http_reqs.values);
+    const traffic_message = `The traffic values are ${traffic}\n`;
   
-//     return {
-//       stdout: latency_message.concat(errors_message, traffic_message),
-//     };
-//   }
+    return {
+      stdout: latency_message.concat(errors_message, traffic_message),
+    };
+  }
