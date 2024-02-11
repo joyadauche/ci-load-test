e86de3d5d18f9b8bc9f5205401492ec395a46001
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-11 15:53:34 -0500

    Check why connection is refused in capture-prometheus-metrics

--- .github/scripts/capture-prometheus-metrics.sh
+++ .github/scripts/capture-prometheus-metrics.sh
@@ -1,5 +1,6 @@
-kubectl port-forward svc/kube-prometheus-stack-prometheus 9090:9090 --namespace monitoring --kubeconfig ~/.kube/config&
-curl -sS http://localhost:9090/api/v1/query\?query\=node_load5
+set -e
+kubectl get svc --namespace monitoring
+kubectl port-forward svc/kube-prometheus-stack-prometheus 9090:9090 --namespace monitoring --kubeconfig ~/.kube/config && curl -sS "http://localhost:9090/api/v1/query\?query\=node_load5"
 ps aux | grep 9090
 result=$(ps aux | grep 9090 | awk '{print $2}' | tr '\n' ', ' | sed 's/, $//')
 echo "Process Ids: $result"
