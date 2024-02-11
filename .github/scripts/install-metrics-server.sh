3175fd758a23bfe13a09c343c999c9070dba0fcc
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-11 17:29:36 -0500

    Add condition to check if metrics server pods are running before upgrade

--- .github/scripts/install-metrics-server.sh
+++ .github/scripts/install-metrics-server.sh
@@ -1,5 +1,8 @@
 # install metrics server
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 helm upgrade --install metrics-server metrics-server/metrics-server
-sleep 15s
-helm upgrade metrics-server metrics-server/metrics-server --set args="{--kubelet-insecure-tls}" -n kube-system
+result=$(kubectl get pods -n kube-system | grep metrics-server | awk 'NR==1')
+echo "Metrics Server Pods Status: $result"
+if [[ "${result}" == "Running" ]]; then
+  helm upgrade metrics-server metrics-server/metrics-server --set args="{--kubelet-insecure-tls}" -n kube-system
+fi
