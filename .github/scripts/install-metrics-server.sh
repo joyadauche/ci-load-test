1a67851020471a8578d0d3b76b5b0451d4f07d1d
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-11 17:19:02 -0500

    Add some wait time for the metrics server to startup before upgrading

--- .github/scripts/install-metrics-server.sh
+++ .github/scripts/install-metrics-server.sh
@@ -1,4 +1,5 @@
 # install metrics server
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 helm upgrade --install metrics-server metrics-server/metrics-server
+sleep 15s
 helm upgrade metrics-server metrics-server/metrics-server --set args="{--kubelet-insecure-tls}" -n kube-system
