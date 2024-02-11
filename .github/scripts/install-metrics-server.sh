f640ba928c62f8e81372436563a216edd4144b84
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-11 17:06:12 -0500

    Install metrics server after cluster have been provisioned

---
+++ .github/scripts/install-metrics-server.sh
@@ -0,0 +1,4 @@
+# install metrics server
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+helm upgrade --install metrics-server metrics-server/metrics-server
+helm upgrade metrics-server metrics-server/metrics-server --set args="{--kubelet-insecure-tls}" -n kube-system
