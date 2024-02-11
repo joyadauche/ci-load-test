89d0f90506e594fba8190f3624781edaaf633f7d
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-02-11 17:44:56 -0500

    Remove unused script, modify helm upgrade command in install-tools script

--- .github/scripts/install-tools.sh
+++ .github/scripts/install-tools.sh
@@ -12,7 +12,7 @@ sudo apt-get install k6
 # install metrics server
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 helm upgrade --install metrics-server metrics-server/metrics-server
-result=$(kubectl get pods -n kube-system | grep metrics-server | awk 'NR==1')
+result=$(kubectl get pods -n kube-system | grep metrics-server | awk 'NR==1' | awk '{print $3}')
 echo "Metrics Server Pods Status: $result"
 if [[ "${result}" == "Running" ]]; then
   helm upgrade metrics-server metrics-server/metrics-server --set args="{--kubelet-insecure-tls}" -n kube-system
